dee915430ee4390d100f1681f001df2d06351988
Dhaya3132 <dhayalanu103@gmail.com>
2024-04-22 08:29:43 +0530

    Updated Look and theme

--- sandbox.js
+++ sandbox.js
@@ -29,3 +29,50 @@ Close.addEventListener('click', ()=>
     Slide.style.display = 'none';
 })
 
+const CVButton = document.getElementById('CV');
+
+CVButton.addEventListener('click',()=>{
+
+    let Pdf = 'https://drive.google.com/file/d/1nCDcFxYKDIxcwqKpv0Nr1tzFEU2CvfZ9/view?usp=drive_link';
+    window.open(Pdf, '_blank');
+})
+
+const Contacts = document.getElementById('MyContact');
+
+Contacts.addEventListener('click',()=>{
+    const Contact = document.getElementById('Contacts');
+
+    Contact.scrollIntoView({ behavior: 'smooth' });
+})
+
+const Abouts = document.getElementById('Aboutmee');
+
+Abouts.addEventListener('click',()=>{
+    const about = document.getElementById('AboutMe');
+
+    about.scrollIntoView({behavior:'smooth'});
+})
+
+const Projects = document.getElementById('Myprojectss');
+
+Projects.addEventListener('click',()=>{
+    const proj = document.getElementById('MyProjects');
+
+    proj.scrollIntoView({behavior:'smooth'});
+})
+
+const aboutsidebar = document.getElementById('AboutmeSidebar');
+
+aboutsidebar.addEventListener('click',()=>{
+    const about = document.getElementById('AboutMe');
+
+    about.scrollIntoView({behavior:'smooth'});
+})
+
+const Projectsidebar = document.getElementById('MyprojectsSidebar');
+
+Projectsidebar.addEventListener('click',()=>{
+    const proj = document.getElementById('MyProjects');
+
+    proj.scrollIntoView({behavior:'smooth'});
+})
